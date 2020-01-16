9532499110eebb17bfa162fb86036d55d4f15e0c
qianfang <qianfang-khjy@bestpay.com.cn>
2020-01-17 00:05:41 +0800

    测试

---
+++ src/main/java/com/example/demo/work.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * @author qianfang
+ * @version Id: work.java, v 0.1 2020/1/17 0:04 qianfang Exp $$
+ */
+public class work {
+    private int id;
+    private int it;
+
+}
